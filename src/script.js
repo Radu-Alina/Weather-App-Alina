f23da76ecf8d31f980cdab3098420485f530dc52
Radu-Alina <alinamomirov@gmail.com>
2022-07-09 13:07:04 +0200

    Updated the weatherapp

---
+++ src/script.js
@@ -0,0 +1,106 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = "6e614b93e63f6a9848310bb4ddaf3fe3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "6e614b93e63f6a9848310bb4ddaf3fe3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function fromCelsiusToFahrenheit(temp) {
+  return Math.round(temp * 1.8 + 32);
+}
+function fromFahrenheitToCelsius(temp) {
+  return Math.round((temp - 32) / 1.8);
+}
+function celsiusLink() {
+  if (!celsiusIsCurrent) {
+    celsiusLink.classList.add("bold");
+    fahrenheitLink.classList.remove("bold");
+    let temp = document.querySelector("#current-temperature");
+    temp.innerHTML = "" + fromFahrenheitToCelsius(temp.textContent);
+    celsiusIsCurrent = true;
+  }
+}
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureCurrent = document.querySelector("#current-temperature");
+  let F = Math.round(29 * 1.8 + 32);
+  temperatureCurrent.innerHTML = F;
+}
+let temperatureFahrenheit = document.querySelector("#Fahrenheit");
+temperatureFahrenheit.addEventListener("click", convertToFahrenheit);
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureCurrent = document.querySelector("#current-temperature");
+  temperatureCurrent.innerHTML = 29;
+}
+let temperatureCelsius = document.querySelector("#Celsius");
+temperatureCelsius.addEventListener("click", convertToCelsius);
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Berlin");
