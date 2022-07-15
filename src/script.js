97a2dd991d7f91ddc8bc212af9f13831f2a367e3
Radu-Alina <alinamomirov@gmail.com>
2022-07-15 13:14:20 +0200

    Added the relevant icon from the Api

--- src/script.js
+++ src/script.js
@@ -28,12 +28,15 @@ function displayWeatherCondition(response) {
     response.data.main.temp
   );
 
+  document.querySelector("#precipitation").innerHTML=response.data.main.precipitation;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  document.querySelector("#icon").setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  icon.setAttribute("alt",response.data.weather[0].description);
 }
 
 function searchCity(city) {
