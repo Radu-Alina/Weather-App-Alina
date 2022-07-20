05f0cfbe33bfaf1fcc2298e49ca5675254fd9070
Radu-Alina <alinamomirov@gmail.com>
2022-07-20 11:53:16 +0200

    corrected the getForecast

--- src/script.js
+++ src/script.js
@@ -56,7 +56,7 @@ function getForecast(coordinates) {
     console.log(coordinates);
     let apiKey = "6e614b93e63f6a9848310bb4ddaf3fe3";
     let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayForecast)
+    axios.get(apiUrl).then(displayForecast);
 }
 
 function displayWeatherCondition(response) {
@@ -75,11 +75,12 @@ function displayWeatherCondition(response) {
   document.querySelector("#icon").setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   icon.setAttribute("alt",response.data.weather[0].description);
 
+  getForecast(response.data.coord);
+
   celsiusTemperature = response.data.main.temp;
 
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  temperatureElement.innerHTML=Math.round(celsiusTemperature);
 
-  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
